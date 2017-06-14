8ac8b4355f25b545ff5b9109f39e6a39ff1f916e
徐坚 <xujianhust@outlook.com>
2017-06-14 23:12:40 +0800

    code

---
+++ testShare/testShare/testShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testShare
+//
+//  Created by Lawrence_Xu on 2017/6/14.
+//  Copyright © 2017年 Lawrence_Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
